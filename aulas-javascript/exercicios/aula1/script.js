ed5f1605c2e3c115343b3772d012732173ab4dfe
Brunna Linhares <brunnalinhares21@gmail.com>
2020-09-22 21:43:35 -0300

    Resolucao do exercicio Aula01-JS: Brunna Alves

---
+++ aulas-javascript/exercicios/aula1/script.js
@@ -0,0 +1,16 @@
+
+function checarSePodeDirigir(){
+    let idade = prompt('Digite a sua idade: ');
+    if (!isNaN(idade)){
+           
+        if (idade > 18){
+            alert('Já pode dirigir!');
+        }else{
+            alert('Ainda não pode dirigir!');
+        }
+
+    }else{
+        alert('O valor informado não é um número')
+    }
+}
+checarSePodeDirigir();
